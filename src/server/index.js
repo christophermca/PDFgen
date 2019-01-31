59cf51df6e6bf06ee244a31a086c733da4b51c56
Christopher MCAdams <mca.christopher@gmail.com>
2019-01-31 10:56:59 -0500

    HCP-5713 json file to set defaults for textarea

--- src/server/index.js
+++ src/server/index.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const mustacheExpress = require('mustache-express')
+const mustacheExpress = require('mustache-express');
+const fs = require('fs');
 
 const app = express();
 const port = 5713;
-const message = 'PDF Generator app is running....'
-const endpointName = 'generate_pdf'
+const message = 'PDF Generator app is running....';
+const endpointName = 'generate_pdf';
+const stubData = require('./views/stubData.json')
 
 // To Use Mustache templating
 app.engine('html.mustache', mustacheExpress());
 app.set('view engine', 'html.mustache');
-app.set('views', __dirname +  '/..' + '/generator/templates/')
+app.set('views', path.resolve(__dirname, '/..'))
 
 // To read req body: needed for POST
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.raw());
 
-// Error Handling
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('UH OH!')
-});
-
-
 // REST API
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/views/index.html'));
+  res.render(__dirname + '/views/index', {data: JSON.stringify(stubData)});
 });
 
 const publicPath = path.resolve(__dirname, '../generator/templates');
@@ -34,7 +29,7 @@ app.use(express.static(publicPath));
 
 app.post(`/${endpointName}`, (req, res) => {
   const json = JSON.parse(req.body['json-data'])
-  res.render('index', json )
+  res.render(__dirname + '/../generator/templates/index', json )
 });
 
 app.listen(port, () => console.log(message));
